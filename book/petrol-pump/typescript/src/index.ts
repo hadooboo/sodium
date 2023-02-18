5564ae1d9cc846b3d870b47567a5bdaa05b119eb
NAM JAEHO <njh215@snu.ac.kr>
2023-02-19 00:51:28 +0900

    prepare chapter 04

---
+++ book/petrol-pump/typescript/src/index.ts
@@ -0,0 +1,7 @@
+class LandingPage {
+  public static main() {
+    // Do nothing.
+  }
+}
+
+LandingPage.main();
---
+++ book/petrol-pump/typescript/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.ts',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      chunks: ['index'],
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 5500,
+  },
+};
